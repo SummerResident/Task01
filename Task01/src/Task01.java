be829f1115b85a1f5d9c33d31f2764c74c213a88
admin <admin@169.254.85.127>
2018-09-08 22:22:52 +0300

    first

---
+++ Task01/src/Task01.java
@@ -0,0 +1,11 @@
+
+public class Task01 {
+
+	public static void main(String[] args) {
+	    System.out.print("ФИО студента: Барталев Павел Эдуардович ");
+	    System.out.print("\nДата рождения:25.09.1998");
+	    System.out.print("\nСпециальность:  \tАвтоматизация компьютерно-интегрированных технологий");
+	    System.out.println("\nКу\bрс: третий курс");
+	  }
+	
+}
